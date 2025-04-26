d497bad5211a2cba58c03c24f669a3c3c38383da
Ivan Vanofis Buzynnyi <synergyivanb@gmail.com>
2025-04-27 00:57:35 +0300

    base files

---
+++ MainWindowForm.cs
@@ -0,0 +1,11 @@
+using System.Windows.Forms;
+
+namespace AncientOnesTranslator;
+
+public partial class MainWindowForm : Form
+{
+    public MainWindowForm()
+    {
+        InitializeComponent();
+    }
+}
