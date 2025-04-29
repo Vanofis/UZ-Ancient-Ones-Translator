d8f4a48b839ca2352ca2e302fc1b06fc4b170f55
Ivan Vanofis Buzynnyi <synergyivanb@gmail.com>
2025-04-29 23:50:33 +0300

    moved double letter to DoubleLetterProcessor renamed StringProcessorUtils to CharExtensions

---
+++ Processors/CharExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace AncientOnesTranslator.Processors;
+
+internal static class CharExtensions
+{
+    public static bool CompareCharsCaseInsensitive(this char c1, char c2)
+    {
+        return char.ToLowerInvariant(c1) == char.ToLowerInvariant(c2);
+    }
+}
--- Processors/DoubleLetterProcessor.cs
+++ Processors/DoubleLetterProcessor.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace AncientOnesTranslator.Processors;
 
 internal record DoubleLetterProcessor : IStringProcessor
 {
+    private Dictionary<char, char> _doubleLettersReplacers = new()
+    {
+        { 'a', 'α' },
+        { 'b', 'β' },
+        { 'c', 'γ' },
+        { 'd', 'δ' },
+        { 'e', 'ε' },
+        { 'f', 'ζ' },
+        { 'g', 'η' },
+        { 'h', 'θ' },
+        { 'i', 'ι' },
+        { 'j', 'ї' },
+        { 'k', 'λ' },
+        { 'l', 'μ' },
+        { 'm', 'ν' },
+        { 'n', 'ξ' },
+        { 'o', 'ο' },
+        { 'p', 'π' },
+        { 'q', 'ρ' },
+        { 'r', 'σ' },
+        { 's', 'τ' },
+        { 't', 'υ' },
+        { 'u', 'φ' },
+        { 'v', 'χ' },
+        { 'w', 'ψ' },
+        { 'x', 'ω' },
+        { 'y', 'у' },
+        { 'z', 'ζ' },
+    };
+    
     public string Process(string input)
     {
         var builder = new StringBuilder();
@@ -13,7 +44,7 @@ internal record DoubleLetterProcessor : IStringProcessor
         {
             if (i + 1 < input.Length && input[i].CompareCharsCaseInsensitive(input[i + 1]))
             {
-                if (input[i].TryGetDoubledLetter(out char replacer))
+                if (TryGetDoubledLetter(input[i], out char replacer))
                 {
                     builder.Append(replacer);
                     i += 2;
@@ -29,5 +60,20 @@ internal record DoubleLetterProcessor : IStringProcessor
         }
             
         return builder.ToString();;
+    }
+    
+    private bool TryGetDoubledLetter(char doubledLetter, out char replacer)
+    {
+        if (!_doubleLettersReplacers.TryGetValue(char.ToLowerInvariant(doubledLetter), out replacer))
+        {
+            return false;
+        }
+
+        if (char.IsUpper(doubledLetter))
+        {
+            replacer = char.ToUpperInvariant(replacer);
+        }
+
+        return true;
     }
 };
